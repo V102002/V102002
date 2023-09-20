190cb79b84c891028ddec275ea740bb92951f938
Vinayak Nair <vssnsvs@gmail.com>
2023-09-20 15:20:27 +0530

    Celing of a Number

--- FirstJavaProgram/src/CelingOfNumber.java
+++ FirstJavaProgram/src/CelingOfNumber.java
@@ -1,9 +1,10 @@
-public class BinarySearch {
+public class CelingOfNumber {
     public static void main(String[] args) {
-        int[] arr={-18,-12,-4,0,2,3,4,15,16,18,22,45};
-        int ans=binarySearch(arr,22);
+        int[] arr={2,3,5,9,14,16,18};
+        int ans=binarySearch(arr,5 );
         System.out.println(ans);
     }
+
     static int binarySearch(int[] arr,int target){
         int start=0;
         int end=arr.length-1;
@@ -17,10 +18,10 @@ public class BinarySearch {
                 end=mid-1;
             }else if (target>arr[mid]) {
                 start=mid+1;
-            }else{
+            }else{//if target=arr[mid]
                     return mid;
             }
         }
-        return -1;
+        return start;
     }
 }
