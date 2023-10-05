03d0d21e81ce5b3f5cffb0775179035bed9db77a
Vinayak Nair <vssnsvs@gmail.com>
2023-10-05 10:17:51 +0530

    Constructor Lab

--- FirstJavaProgram/src/Employee.java
+++ FirstJavaProgram/src/Employee.java
@@ -3,14 +3,52 @@ public class Employee {
     String firstName;
     String lastName;
     int Salary;
-    public static void main(String[] args) {
-        Employee ep=new Employee(8, "Peter", "Tan", 2500);
-    }
-    public Employee(int id,String firstName,String lastName,int Salary){
+    public  Employee(int id,String firstName,String lastName,int Salary){
         this.id=id;
         this.firstName=firstName;
         this.lastName=lastName;
         this.Salary=Salary;
+    }
+    public static void main(String[] args) {
+        Employee ep=new Employee(10,"Peter","Tan",2500);
+        System.out.println(ep);
+        ep.raiseSalary(10);
+        System.out.println(ep);
+        Employee ep2=new Employee(11, "Tom", "Clancy", 10000);
+        System.out.println(ep2);
+        ep2.raiseSalary(5);
+        System.out.println(ep2);
+        
 
     }
+    public int getId(){
+        return id;
+    }
+    public String getFirstName(String FirstName){
+        return firstName;
+    }
+    public String getLastName(String LastName){
+        return lastName;
+    }
+    public int getSalary(int Salary){
+        return Salary;
+    }
+    public String GetName(String FirstName,String LastName){
+        return FirstName+" "+LastName;
+    }
+    public void SetSalary(int Salary){
+        this.Salary=Salary;
+    }
+    public int GetAnnualSalary(int Salary){
+        return Salary*12;
+    }
+    public int raiseSalary(int percentage){
+        this.Salary=Salary+Salary*percentage;
+        return Salary;
+    }
+    public String toString() {
+        return "Employee [ID=" + id + ", Name=" + firstName + " " + lastName + ", Salary=" + Salary + "]";
+    }
+    
+    
 }
